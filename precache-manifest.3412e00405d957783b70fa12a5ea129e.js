1b610a79fca50005d5214ca1a4c026e64deb69ea
ramy <ramy.niranjan@gmail.com>
2020-09-07 20:48:00 +0200

    react-shopping-cart V.1

---
+++ precache-manifest.3412e00405d957783b70fa12a5ea129e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd49d5d6823f92a411848c34bf7b4c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cae3e14c36cd87b4b13b",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "a894633e47c3d823b4e2",
+    "url": "/static/css/main.9f980bc3.chunk.css"
+  },
+  {
+    "revision": "cae3e14c36cd87b4b13b",
+    "url": "/static/js/2.37df95ad.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/static/js/2.37df95ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a894633e47c3d823b4e2",
+    "url": "/static/js/main.725d90d8.chunk.js"
+  },
+  {
+    "revision": "e9a3fbab615e5ceac878",
+    "url": "/static/js/runtime-main.3d234fca.js"
+  }
+]);
